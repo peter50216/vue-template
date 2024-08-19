1402145f72d01a82833e0027cb0df38e5cbf5a62
Peter Shih <peter50216@gmail.com>
2024-08-19 23:05:47 +0800

    fix: disable lint on Window, fix husky deprecated

--- src/window.d.ts
+++ src/window.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vue/macros-global" />
 /// <reference types="@vue-macros/reactivity-transform/macros-global" />
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 declare type Window = {
   // extend the window
 };
